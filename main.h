dde3d670170fb7116aca357480f0fba6ed32b9a3
Ochieng24 <edwine.ochi24@gmail.com>
2023-07-21 09:12:20 -0700

    addtitional changes in main.h

--- main.h
+++ main.h
@@ -12,7 +12,7 @@
 extern char **environ;
 
 char **parse_line(char *line);
-int execute_command(char *command, char **argv);
+int execute_command(char *command, char **arg_list);
 int custom_putchar(char ch);
 void prompt(void);
 void custom_puts(char *str);
@@ -20,8 +20,8 @@ size_t custom_strlen(const char *str);
 void handle_error(const char *message);
 void handle_other_errors(void);
 int custom_strcmp(const char *str1, const char *str2);
-void *custom_memcpy(void *dest, const void *src, size_t n);
-void execute_child_process(char **argv);
+void *my_memcpy(void *destination, const void *source, size_t size);
+void execute_child_process(char **arg_list);
 int custom_atoi(const char *str);
 
 #endif
