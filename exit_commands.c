f8edbef9285cf236c7d4a76be0465a1b5e15cd90
faithleo8 <leofaith8@gmail.com>
2023-07-26 18:21:40 +0300

    changes made

--- exit_commands.c
+++ exit_commands.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * parse_line - Parses a line into an array of tokens.
+ * navigate_line - Parses a line into an array of tokens.
  * @line: The input line to parse.
  * Return: An array of tokens.
  */
-char **parse_line(char *line)
+char **navigate_line(char *line)
 {
 const char *delimiters = " \t\r\n\a";
 char **token_array = NULL;/* Array to store tokens*/
@@ -34,19 +34,19 @@ token_array[token_count] = NULL;
 return (token_array);/*Return the array of tokens*/
 }
 /**
- * execute_command - Executes a command with its arguments.
+ * implement_command - Executes a command with its arguments.
  * @command: The command to execute.
  * @arg_list: An array of arguments for the command.
  * Return: 0 on success, -1 on failure.
  */
-int execute_command(char *command, char **arg_list)
+int implement_command(char *command, char **arg_list)
 {
-if (custom_strcmp(command, "exit") == 0)
+if (convention_strcmp(command, "exit") == 0)
 {
 if (arg_list[1] != NULL)
 {
 /*If an argument is provided, convert it to an integer and exit*/
-int exit_status = custom_atoi(arg_list[1]);
+int exit_status = convention_atoi(arg_list[1]);
 exit(exit_status);
 }
 else
@@ -55,14 +55,14 @@ else
 exit(EXIT_SUCCESS);
 }
 }
-else if (custom_strcmp(command, "env") == 0)
+else if (convention_strcmp(command, "env") == 0)
 {
 /*Print the environment variables*/
 char **env = environ;
 while (*env != NULL)
 {
-custom_puts(*env);
-custom_putchar('\n');
+convention_puts(*env);
+convention_putchar('\n');
 env++;
 }
 return (0);
@@ -70,15 +70,15 @@ return (0);
 else
 {
 /* Execute the command as a child process*/
-execute_child_process(arg_list);
+implement_child_procedure(arg_list);
 }
 return (0);
 }
 /**
- * execute_child_process - Executes the child process with the given arguments.
+ * implement_child_procedure - Executes the child process.
  * @arg_list: An array of arguments for the command.
  */
-void execute_child_process(char **arg_list)
+void implement_child_procedure(char **arg_list)
 {
 pid_t child_pid = fork();
 int status;
@@ -104,12 +104,12 @@ size_t path_len, command_len;
 
 while ((path_token = strtok(path_env, ":")) != NULL)
 {
-path_len = custom_strlen(path_token);
-command_len = custom_strlen(command);
+path_len = convention_strlen(path_token);
+command_len = convention_strlen(command);
 /* Construct the full path of the command */
-custom_memcpy(command_path, path_token, path_len);
+my_memcpy(command_path, path_token, path_len);
 command_path[path_len] = '/';
-custom_memcpy(command_path + path_len + 1, command, command_len);
+my_memcpy(command_path + path_len + 1, command, command_len);
 command_path[path_len + command_len + 1] = '\0';
 
 execve(command_path, arg_list, environ);
@@ -141,7 +141,7 @@ dest_ptr[i] = src_ptr[i];/*Copy each byte from source to destination*/
 return (destination);/*Return pointer to the destination memory area*/
 }
 /**
- * custom_atoi - Converts a string to an integer.
+ * convention_atoi - Converts a string to an integer.
  * @str: The input string to convert.
  *
  * This function converts the given string @str to an integer.
@@ -150,7 +150,7 @@ return (destination);/*Return pointer to the destination memory area*/
  *
  * Return: The converted integer value.
  */
-int custom_atoi(const char *str)
+int convention_atoi(const char *str)
 {
 int output = 0;
 int sign = 1;
@@ -175,7 +175,7 @@ index++;
 /*Conversion of digits to integer value*/
 while (str[index] >= '0' && str[index] <= '9')
 {
-result = (output * 10) + (str[index] - '0');
+output = (output * 10) + (str[index] - '0');
 index++;
 }
 return (sign *output);
--- nect-main.c
+++ nect-main.c
@@ -1,19 +1,24 @@
 #include "main.h"
-
 /**
- * prompt - Displays the command prompt.
+ * display_shell_prompt - Displays the command prompt.
+ * This func is responsible for displaying command prompt on screen.
+ * If the standard input is associated with a terminal (interactive mode),
+ * it will print the prompt.
+ * The prompt format is: " $ ".
  */
-void prompt(void)
+void display_shell_prompt(void)
 {
 	/* Display the prompt */
 	if (isatty(STDIN_FILENO) == 1)
 	{
-		custom_puts(" $ ");
+		convention_puts(" $ ");
 	}
 }
 /**
  * handle_error - Handles error conditions and exits.
  * @message: The error message to display.
+ * This func handles error conditions by printing an error msg
+ * and terminating the program with a failure exit code.
  */
 void handle_error(const char *message)
 {
@@ -22,15 +27,19 @@ void handle_error(const char *message)
 	exit(EXIT_FAILURE);
 }
 /**
- * handle_other_errors - Handles other error conditions.
+ * handle_misc_errors - Handles other error conditions.
+ * This func is responsible for handling miscellaneous error conditions
+ * that may arise during the program's execution
+.* Handles the specific error case of a line being too long and prints
+ * an error message to the standard error output.
  */
-void handle_other_errors(void)
+void handle_misc_errors(void)
 {
 	/* Handle other error conditions, such as line length exceeding a limit */
-	const char error_message[] = "Error: Line too long\n";
-	size_t message_len = custom_strlen(error_message) - 1;
+	const char error_msg[] = "Error: Line too long\n";
+	size_t msg_length = convention_strlen(error_msg) - 1;
 
-	write(STDERR_FILENO, error_message, message_len);
+	write(STDERR_FILENO, error_msg, msg_length);
 }
 /**
  * main - Entry point of the program.
@@ -38,36 +47,36 @@ void handle_other_errors(void)
  */
 int main(void)
 {
-	char *line = NULL;
+	char *user_input = NULL;
 	ssize_t read_status;
-	size_t bf = 0;
-	char **argv = NULL;
+	size_t buffer_size = 0;
+	char **cmd_args = NULL;
 
 	while (1)
 	{
-		prompt();
-		read_status = getline(&line, &bf, stdin);
+		display_shell_prompt();
+		read_status = getline(&user_input, &buffer_size, stdin);
 		if (read_status == -1)
 		{
-			free(line);
+			free(user_input);
 			exit(EXIT_SUCCESS);
 		}
 		else if (read_status == -2)
 		{
-			handle_other_errors();
+			handle_misc_errors();
 			continue;  /* Skip executing the command and go back to the prompt */
 		}
 		else
 		{
-			line[read_status - 1] = '\0'; /* Remove the newline character */
-			argv = parse_line(line);
-			if (argv[0] != NULL)
+			user_input[read_status - 1] = '\0'; /* Remove the newline character */
+			cmd_args = navigate_line(user_input);
+			if (cmd_args[0] != NULL)
 			{
-				execute_command(argv[0], argv);
+				implement_command(cmd_args[0], cmd_args);
 			}
-			free(argv);
+			free(cmd_args);
 		}
 	}
-	free(line);
+	free(cmd_args);
 	return (0);
 }
